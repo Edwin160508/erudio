48e386a4c8a219df5c46d05cae7522ef6ef50230
Edwin Lima <edwin.limaa@gmail.com>
2022-12-06 16:03:28 -0300

    Integration test with Junit, testing vo pattern converter.

---
+++ 09_WorkingWithVOPattern/erudio/src/test/java/br/com/erudio/mapper/mocks/MockPerson.java
@@ -0,0 +1,59 @@
+package br.com.erudio.mapper.mocks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.erudio.data.model.Person;
+import br.com.erudio.data.vo.v1.PersonVO;
+
+public class MockPerson {
+	public List<Person> mockEntityList(){
+		List<Person> entities = new ArrayList<>();
+		int index = 0;
+		while(index < 14) {
+			entities.add(mockEntity(index));
+			index++;
+		}
+		
+		return entities;
+	}
+	
+	public Person mockEntity() {
+		return mockEntity(0);
+	}
+	
+	public Person mockEntity(Integer number) {
+		Person entity = new Person();
+		entity.setFirstName("FirstName Test "+number);
+		entity.setLastName("LastName Test " + number);
+		entity.setAddress("Addres Test "+number);
+		entity.setGender((number % 2)==0 ? "Male":"Female");
+		entity.setId(number.longValue());
+		return entity;
+	}
+	
+	public PersonVO mockVO() {
+		return mockVO(0);
+	}
+	
+	public PersonVO mockVO(Integer number) {
+		PersonVO vo = new PersonVO();
+		vo.setFirstName("FirstName Test "+number);
+		vo.setLastName("LastName Test " + number);
+		vo.setAddress("Addres Test "+number);
+		vo.setGender((number % 2)==0 ? "Male":"Female");
+		vo.setId(number.longValue());
+		return vo;
+	}
+	
+	public List<PersonVO> mockVOList(){
+		List<PersonVO> entities = new ArrayList<>();
+		int index = 0;
+		while(index < 14) {
+			entities.add(mockVO(index));
+			index++;
+		}
+		
+		return entities;
+	}
+}
